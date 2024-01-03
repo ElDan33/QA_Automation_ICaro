65e79b37556775400df033e0ffc64057e550145a
Dante Carrizo <danteenriquecarrizo@gmail.com>
2024-01-03 00:38:27 -0300

    Se modificó el src

--- src/main/java/org/DanCa/Clase1/Practice1.java
+++ src/main/java/org/DanCa/Clase1/Practice1.java
@@ -3,5 +3,6 @@ package org.DanCa.Clase1;
 public class Practice1 {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("My first code in Java");
     }
 }
