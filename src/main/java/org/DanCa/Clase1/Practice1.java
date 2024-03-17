5c6114041b022baa3236a96548cc07cda6132032
Dante Carrizo <danteenriquecarrizo@gmail.com>
2024-03-17 11:31:02 -0300

    Delete src/main/java/org/DanCa/Clase1 directory
    Directorio sobrante descartado

--- src/main/java/org/DanCa/Clase1/Practice1.java
+++
@@ -1,8 +0,0 @@
-package org.DanCa.Clase1;
-
-public class Practice1 {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-        System.out.println("My first code in Java");
-    }
-}
